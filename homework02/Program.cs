9e9e09341c80b7a9451ef9791aa6dbbb8be11cc2
stanislav <stanislavmuratov@yandex.ru>
2022-09-18 19:56:33 +0300

    Add is even number?

--- homework02/Program.cs
+++ homework02/Program.cs
@@ -1,4 +1,5 @@
 ﻿// homework _1 (Задача 4):
+// largest of three
 Console.WriteLine("Введите три числа: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
--- homework03/Program.cs
+++ homework03/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// homework _1 (Задача 6):
+// is even number?
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
