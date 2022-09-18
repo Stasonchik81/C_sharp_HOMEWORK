ec4eaba394ead3da2829c3379b464848e293fadd
stanislav <stanislavmuratov@yandex.ru>
2022-09-18 12:53:06 +0300

    Первая задача - большее число из двух

--- homework01/Program.cs
+++ homework01/Program.cs
@@ -1,2 +1,9 @@
-﻿// homework _1
+﻿// homework _1 (Задача 2):
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2) Console.WriteLine($"Число {num1} - большее.");
+else if (num2 > num1) Console.WriteLine($"Число {num2} - большее.");
+else Console.WriteLine("Числа равны!");
 
