67c82610fa8cee6dda35b3068bef83ca7c1c5bdf
stanislav <stanislavmuratov@yandex.ru>
2022-09-18 21:20:16 +0300

    even numbers from 1 to N

---
+++ homework04/Program.cs
@@ -0,0 +1,28 @@
+﻿// homework _1 (Задача 8):
+// even numbers from 1 to N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// _______Вывод в обратном порядке_______
+// while (num > 0)
+// {
+//     if (num % 2 == 0)
+//     {
+//         Console.Write($"{num}, ");
+//     }
+//     num--;
+// }
+
+for (int i = 1; i <= num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        if(i!=num)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
