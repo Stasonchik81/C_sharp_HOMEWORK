2765830a1043955c5718021ccebe52fd8782c45e
stanislav <stanislavmuratov@yandex.ru>
2022-09-18 13:28:53 +0300

    largest of three

---
+++ homework02/Program.cs
@@ -0,0 +1,13 @@
+﻿// homework _1 (Задача 4):
+Console.WriteLine("Введите три числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if(num2>max){
+    max = num2;
+}
+else if(num3>max){
+    max = num3;
+}
+Console.WriteLine($"Наибольшее число - {max}!");
