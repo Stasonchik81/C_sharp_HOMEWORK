3a899910afeacc301be76dfd6726d5b6896af97a
stanislav <stanislavmuratov@yandex.ru>
2022-09-20 22:42:12 +0300

    get third digit

--- homework12/Program.cs
+++ homework12/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100) 
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+while (num > 1000)
+{
+    num /=10;
+}
+Console.WriteLine(getThirdDigit(num));
+
+int getThirdDigit(int num)
+{
+    return num % 10;
+}
