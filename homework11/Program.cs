8f12a1e5ebda424489f4be0d570c9f728392129d
stanislav <stanislavmuratov@yandex.ru>
2022-09-20 22:07:37 +0300

    Get second digit

--- homework11/Program.cs
+++ homework11/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 999 || num < 100) Console.WriteLine("Некорректный ввод!");
+else Console.WriteLine(getSecondDigit(num));
+
+int getSecondDigit(int num)
+{
+    return (num / 10) % 10;
+}
