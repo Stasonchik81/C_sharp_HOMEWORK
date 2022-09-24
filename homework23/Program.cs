56831d685300561fa3d89bfbcd734e11795eaf42
stanislav <stanislavmuratov@yandex.ru>
2022-09-24 11:09:49 +0300

    Final changing of cubing

---
+++ homework23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void printListCubing(int num)
+{
+    Console.WriteLine($"Таблица кубов числа {num}");
+    for (int i = 1; i <= num; i++)
+    {
+        // if(i == num)Console.Write($"{Math.Pow(i, 3)}"); вывод в строку
+        // else Console.Write($"{Math.Pow(i, 3)},");
+
+        Console.WriteLine($"{i} | {Math.Pow(i, 3)}");
+    }
+}
+
+printListCubing(number);
