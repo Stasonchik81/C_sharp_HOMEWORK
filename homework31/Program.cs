ee2bb21e1ef76f824730ee1eb410a6f08fb22f47
stanislav <stanislavmuratov@yandex.ru>
2022-09-27 11:58:14 +0300

    Add homework 31 - degree of number

---
+++ homework31/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите два числа: ");
+Console.Write("Основание: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Степень: ");
+int powB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"основание - {numberA}, степень - {powB}, result: {Pow(numberA, powB)}");
+int Pow (int a, int b)
+{
+    int res = 1;
+    while (b>0)
+    {
+        res *= a;
+        b--;
+    }
+    return res;
+}
+
