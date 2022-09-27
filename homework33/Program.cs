cb2c1edce5363ef6e031b8c7da11b00897b14fdb
stanislav <stanislavmuratov@yandex.ru>
2022-09-27 14:15:39 +0300

    Add get random array

--- homework33/Program.cs
+++ homework33/Program.cs
@@ -2,4 +2,27 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine("Hello, World!");
+int[] myArray = GetRandomArray(8, 10);
+PrintArray(myArray);
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == array.Length - 1) Console.Write($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.Write(" ]");
+}
+
+int[] GetRandomArray(int size, int max)
+{
+    Random rnd = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, max);
+    }
+    return arr;
+}
