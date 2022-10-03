41c1386c1b78f52fd16e157f4e22c9d4a41cebea
stanislav <stanislavmuratov@yandex.ru>
2022-10-03 23:59:36 +0300

    first step of homework

---
+++ homework51/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа разделяя пробелом: ");
+string input = Console.ReadLine();
+int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+
+PrintArray(numbers);
+Console.WriteLine($" => {CountPositiveNum(numbers)}");
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.Write(" ]");
+}
+int CountPositiveNum(int[] arr)
+{
+    int count = 0;
+    foreach (var item in arr)
+    {
+        if(item>0) count++;
+    }
+    return count;
+}
---
+++ homework52/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите данные: ");
+Console.Write("b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"b1={b1}, k1={k1}, b2={b2}, k2={k2}");
+double[] intPoint = IntersectionPoint(b1, k1, b2, k2);
+
+double[] IntersectionPoint(int b1, int k1, int b2, int k2)
+{
+    double x, y;
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    double[] point = new double[2] {x, y};
+    return point;
+}
+Console.WriteLine($"Точка пересечения двух прямых: y = {k1} * x + {b1} и y = {k2} * x + {b2} => ({intPoint[0]}; {intPoint[1]})");
